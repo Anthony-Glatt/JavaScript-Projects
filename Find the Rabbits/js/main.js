88918ca61a20fb3dcedf43657688f59c144e156f
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-11-01 16:59:50 -0700

    Updates

--- Find the Rabbits/js/main.js
+++ Find the Rabbits/js/main.js
@@ -2,6 +2,9 @@ let snakeLocation = Math.floor(Math.random() * 8) + 1;
 
 function behindTheBox(number, snakeLocation) {
   if (number == snakeLocation) {
-    document.getElementById(number).style.backgroundColor = "red";
-  } else document.getElementById(number).style.backgroundColor = "blue";
+    document.getElementById(number).style.backgroundImage =
+      "url(./img/snake.png)";
+  } else
+    document.getElementById(number).style.backgroundImage =
+      "url(./img/rabbit.png)";
 }
