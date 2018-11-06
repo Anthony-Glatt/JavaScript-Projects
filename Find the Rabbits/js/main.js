4afd48af54351770de03890ed2c0c7e7846e49bc
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2018-11-05 17:08:14 -0800

    Updates

--- Find the Rabbits/js/main.js
+++ Find the Rabbits/js/main.js
@@ -1,13 +1,16 @@
 let snakeLocation = Math.floor(Math.random() * 8) + 1;
 
 function behindTheBox(number, snakeLocation) {
+  let counter = 0;
   if (number == snakeLocation) {
     document.getElementById(number).style.backgroundImage =
       "url(./img/snake.png)";
     setTimeout(lostGame, 700);
-  } else
+  } else {
     document.getElementById(number).style.backgroundImage =
       "url(./img/rabbit.png)";
+    counter++;
+  }
 }
 
 function lostGame() {
