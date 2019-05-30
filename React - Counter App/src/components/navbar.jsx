72b1a79d0d01409b93e60375c7b64a235560855d
Anthony-Glatt <40805126+Anthony-Glatt@users.noreply.github.com>
2019-05-29 20:26:21 -0700

    More practice
    Learning:
    Lifting the state up
    Stateless functional components
    Destructuring arguments
    Lifecycle hooks

--- React - Counter App/src/components/navbar.jsx
+++ React - Counter App/src/components/navbar.jsx
@@ -1,1 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
+
+// Stateless Functional Component (sfc like imrc)
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill secondary">{totalCounters}</span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
